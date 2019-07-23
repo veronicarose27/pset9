12d433b260d61a2e3708fc570adcd0252f8c4038
veronicarose27 <52283415+veronicarose27@users.noreply.github.com>
2019-07-23 11:44:52 +0530

    Update 199.py

--- 199.py
+++ 199.py
@@ -1,11 +1,15 @@
 l=['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m']
 p=input()
-for i in l:
-    x=p+i
-    h=x[::-1]
-    if(x==h):
-        print("yes")
-        break
-    
+k=p[::-1]
+if(p==k):
+    print("yes")
 else:
-    print("no")
+    for i in l:
+        x=p+i
+        h=x[::-1]
+        if(x==h):
+            print("yes")
+            break
+    
+    else:
+        print("no")
